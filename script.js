c3caa8f4fcddf2659257417b01df41da272ff18e
GUSTAVO DE FARIA <133820578+GustavodFaria@users.noreply.github.com>
2023-11-09 17:09:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let btnNav = document.querySelector('#menu-btn')
 let listaNav = document.querySelector('.navbar-list')
 
@@ -6,3 +7,15 @@ btnNav.onclick = () =>{
     listaNav.classList.toggle('active')
 }
 
+ScrollReveal().reveal('.headline', { delay: 300 });
+ScrollReveal().reveal('.headline2', { delay: 300 });
+ScrollReveal().reveal('.tagline', { delay: 400 });
+ScrollReveal().reveal('.fila1', { delay: 400 });
+ScrollReveal().reveal('.punchline', { delay: 500 });
+ScrollReveal().reveal('.botonline', { delay: 500 });
+ScrollReveal().reveal('.fila2', { delay: 500 });
+
+ScrollReveal().reveal('.livro1', { delay: 400 });
+ScrollReveal().reveal('.livro2', { delay: 500 });
+ScrollReveal().reveal('.livro3', { delay: 600 });
+ScrollReveal().reveal('.livro4', { delay: 700 });
